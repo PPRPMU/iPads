81cfcf6eb40b5684bb8c291a42000e87f981b3ef
mirosado <miguel@Andrews-Mac-mini.local>
2017-07-07 15:40:16 -0400

    Filtering Aquatics
    Filters Aquatics Audit Control documents. Moves folder (eventually will
    move to correct district folder).

---
+++ filter.py
@@ -0,0 +1,11 @@
+ with open(fileName) as f:
+                doc = slate.PDF(f)
+                text = str(doc)
+                title = text[2:59]
+                field_list = []
+                    if title = "PHILADELPHIA PARKS & RECREATION\n\nAQUATICS AUDIT CONTROL"
+                        shutil.move(f, "P:/PERFORMANCE MGMT/Projects/Test Python/" + str(fileName) + ".PDF")
+                        for x in y:
+                            f
+                    # else if title = "site visit"
+                        # shutil.move(f, "destination")
